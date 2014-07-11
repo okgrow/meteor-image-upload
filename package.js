24dedc0549d15377dbb30fd8faa108364e899ee2
Paul Dowman <paul@pauldowman.com>
2014-07-11 11:01:49 -0400

    Use package.imply to install related packages

--- package.js
+++ package.js
@@ -5,4 +5,11 @@ Package.describe({
 Package.on_use(function (api) {
   api.add_files('lib/easy_cfs_images.js', ['server', 'client']);
   api.export('ImageCollectionFactory');
+  api.imply([
+    'collectionFS',
+    'cfs-s3',
+    'cfs-tempstore',
+    'cfs-filesystem',
+    'cfs-graphicsmagick'
+  ]);
 });

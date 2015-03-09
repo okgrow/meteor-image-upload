8292d9c787cd12dd5e826cd24c397b87395b20cf
rgould <rwgould@gmail.com>
2015-03-09 12:35:59 -0700

    Bump minor version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name:    "okgrow:image-upload",
-  version: "0.5.0",
+  version: "0.5.1",
   summary: "Let users upload images in your app",
   git:     "https://github.com/okgrow/meteor-image-upload/"
 });
